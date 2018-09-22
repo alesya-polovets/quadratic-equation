175ea540f106e8d83b097c118fec3ba00c6edd15
Alesya Polovets <alesya.polovets@gmail.com>
2018-09-22 15:12:37 +0300

    quadratic-equation/feature: implemented solveEquation function, wihch solves Quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
   // your implementation
-}
+    let arr, a, b, c, d, x1, x2, solutions;
+    arr = equation.split(' ');
+    a = +arr[0];
+    if (arr[3] == '-') {
+        b = +arr[4] * -1;
+    }
+    else {
+        b = +arr[4];
+    }
+    if (arr[7] == '-') {
+        c = +arr[8] * -1;
+    }
+    else {
+        c = +arr[8];
+    }
+    d = b * b - 4 * a * c;
+    if (d > 0) {
+        x1 = Math.round((- b + Math.sqrt(d)) / (2 * a));
+        x2 = Math.round((- b - Math.sqrt(d)) / (2 * a));
+    }
+    solutions = [x1, x2].sort(function (a, b) {
+        return a - b;
+    });
+    return solutions;
+};
